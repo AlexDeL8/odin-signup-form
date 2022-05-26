2744009164ef04bfd407c01a37332b4a8e743203
Alex De Luca <alexnaj88@gmail.com>
2022-05-26 19:04:12 -0400

    adding final styling with pure html5 validation

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function formDoNothing() {
+const formSubmitButton = document.getElementById("form-submit-btn");
+formSubmitButton.addEventListener("click", doNothingSubmit);
+
+function doNothingSubmit() {
+    console.log("Submit Click");
+    const passwordValue = document.getElementById("password").value;
+    const passwordConfirmValue = document.getElementById("password-confirm").value;
+
+    if (passwordValue !== passwordConfirmValue) {
+        console.log(passwordValue, passwordConfirmValue);
+        return false;
+    }
     return false;
 }
